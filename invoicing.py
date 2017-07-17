f615f05d25595a285713f2417541b0653a814012
jimmychimmyy <jimmychimmyy@gmail.com>
2017-07-17 12:31:11 -0700

    invoicing.py can write to excel now

--- invoicing.py
+++ invoicing.py
@@ -1,6 +1,7 @@
 #!usr/bin/python
 
 import subprocess
+import xlsxwriter #sudo pip install XlsxWriter
 import os
 
 cwd = os.getcwd()
@@ -11,6 +12,19 @@ result = os.system('tesseract ' + filepath + ' out')
 print(result)
 
 with open("/Users/jimchen/Development/ValleyVisionInventory/out.txt", "r") as myfile:
-	data = list(line for line in (l.strip() for l in myfile) if line)
-	print(data)
+	data = list(line for line in (l.decode('utf-8').strip() for l in myfile) if line)
+	#print(data)
 	os.system('rm /Users/jimchen/Development/ValleyVisionInventory/out.txt')
+
+	workbook = xlsxwriter.Workbook('out.xlsx')
+	worksheet = workbook.add_worksheet()
+
+	for i, string in enumerate(data):
+		print(string)
+		try:
+			worksheet.write(i, 0, str(string))
+		except:
+			print("err")
+	workbook.close()
+
+
